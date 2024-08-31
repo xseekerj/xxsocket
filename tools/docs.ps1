a65b0af558b12952614a68ab65230ce83898dd0e
halx99 <halx99@live.com>
2024-08-31 14:02:46 +0800

    Prepare release 4.3.0 (#449)

--- tools/docs.ps1
+++ tools/docs.ps1
@@ -9,28 +9,23 @@ mike delete --all
 
 Write-Host "Active versions: $rel_str"
 
-if ("$rel_str" -eq "") {
-    Write-Error "docs versions list can't be empty, should be 'docs_ver[:docs_tag],...'"
-    exit 1
-}
-
-$rel_arr = ($rel_str -split ',')
-
 $docs_ver = $null
-foreach($rel in $rel_arr) {
-    # ver:tag
-    $info = ($rel -split ':')
-    $docs_ver = $info[0]
-    $docs_tag = if ($info.Count -ge 2) { $info[1] } else { "v$($info[0])" }
-    git checkout "$docs_tag"
-    mike deploy $docs_ver
+if ($rel_str) {
+    $rel_arr = ($rel_str -split ',')
+    foreach($rel in $rel_arr) {
+        # ver:tag
+        $info = ($rel -split ':')
+        $docs_ver = $info[0]
+        $docs_tag = if ($info.Count -ge 2) { $info[1] } else { "v$($info[0])" }
+        git checkout "$docs_tag"
+        mike deploy $docs_ver
+    }
 }
 
 git checkout dev
 mike deploy latest
 mike list
 
-if ($env:GITHUB_ACTIONS -eq 'true') {
+if ($docs_ver -and ($env:GITHUB_ACTIONS -eq 'true')) {
     mike set-default $docs_ver --push
 }
-
